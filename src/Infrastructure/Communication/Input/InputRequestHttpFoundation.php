c98f07fce113239c2cfc695f4c6353456a781d33
xavi <xavi.developer@gmail.com>
2020-02-02 18:11:06 +0100

    remove illuminate

--- src/Infrastructure/Communication/Input/InputRequestHttpFoundation.php
+++ src/Infrastructure/Communication/Input/InputRequestHttpFoundation.php
@@ -5,16 +5,15 @@ namespace Osds\DDDCommon\Infrastructure\Communication\Input;
 class InputRequestHttpFoundation implements InputRequestInterface
 {
     
-    public function __construct($request)
+    public function __construct()
     {
-        $this->handler = $request;
+        //add httpfoundation handler to construct
         #TODO: use HttpFoundation instead of this
         $this->parameters = $_REQUEST;
     }
 
     public function getParameter($param)
     {
-//        return $this->handler->get($param);
         #TODO: use HttpFoundation instead of this
         if(isset($this->parameters[$param])) {
             return $this->parameters[$param];
@@ -25,7 +24,6 @@ class InputRequestHttpFoundation implements InputRequestInterface
 
     public function getParameters()
     {
-//        return $this->getAll();
         #TODO: use HttpFoundation instead of this
         return $this->parameters;
     }
